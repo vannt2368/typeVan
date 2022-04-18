eaa2eddb04d8ba4d51bbcb14181246fe1736676f
vannt2368 <vannt.bt@gmail.com>
2022-04-18 10:01:26 +0700

    upload code

---
+++ resources/views/typeVan_admin/type.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="refresh" content="0;url=pages/type.php">
+    <link href="css/*.css" rel="stylesheet" type="text/css" media="all">
+    <title>Startmin</title>
+    <script language="javascript">
+        window.location.href = "pages/type.php"
+    </script>
+</head>
+<body>
+<a href="pages/type.php">Go to Demo</a>
+</body>
+</html>
